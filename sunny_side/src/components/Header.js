d506a270031297c71ec20b84abf4d34560e60e93
Diedland <44755255+Diedland@users.noreply.github.com>
2021-06-13 15:28:47 +0200

    production_day_1

---
+++ sunny_side/src/components/Header.js
@@ -0,0 +1,14 @@
+function Header() {
+  return (
+    <div className="header">
+      <h2>sunnyside</h2>
+      <div>
+        <p>About</p>
+        <p>Services</p>
+        <p>Projects</p>
+        <b id="contact">Contact</b>
+      </div>
+    </div>
+  );
+}
+export default Header;
---
+++ sunny_side/src/components/WelcomeSection.js
@@ -0,0 +1,10 @@
+import arrowDown from "../icon-arrow-down.svg";
+function WelcomeSection() {
+  return (
+    <div className="welcome">
+      <h1>WE ARE CREATIVES</h1>
+      <img src={arrowDown} id="down-arrow"></img>
+    </div>
+  );
+}
+export default WelcomeSection;
